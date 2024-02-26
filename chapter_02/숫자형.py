73b2a61e4e2db8047324c08ceb782373234cae65
sejun park <bsejun07@gmail.com>
2024-02-26 21:26:51 +0900

    숫자형 끝

---
+++ chapter_02/숫자형.py
@@ -0,0 +1,78 @@
+print('정수형')
+a=123
+print(a)
+a = -178
+print(a)
+a = 0
+print(a)
+print('------------')
+
+print('실수형')
+
+a = -1.2
+print(a)
+a = -3.45
+print(a)
+
+a = 4.24E10
+print(a)
+a = 4.2e-10
+print(a)
+
+print('------------')
+
+print('8진수와 16진수')
+
+a = 0o177
+print(a)
+
+a = 0x8ff
+print(a)
+
+b = 0xABC
+print(b)
+
+print('-----------')
+
+print('사칙연산')
+a = 3
+print(a)
+b = 4
+print(b)
+a + b
+print(a + b )
+
+a * b
+print(a * b)
+a / b
+print(a / b)
+
+print('-----------')
+
+print('x의 y제곱을 나타내는 ** 연산자')
+
+a = 3
+print(a)
+b = 4
+print(b)
+a ** b
+print(a ** b)
+
+print('---------------')
+
+print('나눗셈 후 나머지를 반환하는 % 연산자')
+
+7 % 3
+print(7 % 3)
+3 % 7
+print(3 % 7)
+
+print('--------')
+
+print('나눗셈 후 몫을 반환하는 // 연산자')
+
+7 / 4
+print(7 / 4)
+
+7 // 4
+print(7 // 4)
